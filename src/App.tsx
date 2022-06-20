b4e1d32380bcf6680d40bc2fb354959b4f7e4650
이민우 <imin-u@imin-uui-MacBookPro-2.local>
2022-06-20 19:48:54 +0900

    feat(price): atom price 구현
    - 마켓 원화 표시
    - 등락에 따른 css style 표시

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
+/* eslint-disable camelcase */
 import React, { useState } from 'react'
 import { useObservableState } from 'observable-hooks'
 import logo from './logo.svg'
 import './App.css'
 import Button from './atoms/Button'
 import { upbitWithLmw$ } from './features/upbit'
+import Price from './atoms/Price'
 
 function App() {
  const deck = useObservableState(upbitWithLmw$, [])
 
  return (
   <div className="App">
-   {deck.map((coin) => (
-    <div key={coin.english_name}>{coin.korean_name}</div>
-   ))}
+   {deck.map(
+    ({
+     english_name,
+     korean_name,
+     trade_price,
+     signed_change_rate,
+     signed_change_price,
+     acc_trade_price_24h,
+     change,
+    }) => (
+     <div key={english_name}>
+      {korean_name}
+      <Price price={trade_price} options={{ currency: 'KRW', symbol: 'ko-KR' }} change={change} />
+      <div>
+       <div>{(signed_change_rate * 100).toFixed(2)}%</div>
+       <div>{signed_change_price.toLocaleString()}</div>
+      </div>
+      <div>{acc_trade_price_24h}</div>
+     </div>
+    ),
+   )}
   </div>
  )
 }
--- src/atoms/Price/index.tsx
+++ src/atoms/Price/index.tsx
@@ -1,13 +1,46 @@
+import styled from '@emotion/styled'
 import React from 'react'
+
+type StyleProps = {
+ RISE: '#c84a31' | string
+ EVEN: '#333' | string
+ FALL: '#1261c4' | string
+ DEFAULT: '#333' | string
+}
+
+const PriceBlock = styled.div`
+ letter-spacing: 2px;
+ color: ${(props: { change: keyof StyleProps; customColor: Partial<StyleProps> }) => props.customColor[props.change]};
+`
 
 interface PriceProps {
  price: number | string
+ options?: {
+  symbol: 'ko-KR'
+  currency: 'KRW'
+ }
+ change?: keyof StyleProps
+ color?: Partial<StyleProps>
 }
 
 function Price(props: PriceProps) {
- const { price } = props
- //  return <div>{new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW' }).format(+price)}</div>
- return <div>{price}</div>
+ const {
+  price,
+  options: { symbol = 'ko-KR', currency = 'KRW' } = {},
+  change = 'DEFAULT',
+  color = {
+   RISE: '#c84a31',
+   EVEN: '#333',
+   FALL: '#1261c4',
+   DEFAULT: '#333',
+  },
+ } = props
+
+ return (
+  <PriceBlock change={change} customColor={color}>
+   {new Intl.NumberFormat(symbol, { style: 'currency', currency }).format(+price)}
+  </PriceBlock>
+ )
 }
 
 export default Price
